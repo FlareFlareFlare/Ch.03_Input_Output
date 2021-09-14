087622aa957ec35819f95f5318a2b6165733287c
FlareFlareFlare <89470789+FlareFlareFlare@users.noreply.github.com>
2021-09-14 10:58:55 -0500

    Update 3.1_Temperature.py

--- 3.1_Temperature.py
+++ 3.1_Temperature.py
@@ -9,9 +9,12 @@ In: 32  Out: 0
 In: 212  Out: 100
 In: 52  Out: 11.1
 In: 25  Out: -3.9
-In: -40  Out: ??? Please tell me what this output is!
+In: -40  Out: -40 Please tell me what this output is!
 
 '''
+temp = float(input("What is the temerature in farenhight?"))
+cel = (temp - 32) * 5/9
+print(int(cel))
 
 
 
