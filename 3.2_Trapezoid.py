23e2512ffa7d76b4d2a9858fa2507a13a0d6faa2
FlareFlareFlare <89470789+FlareFlareFlare@users.noreply.github.com>
2021-09-14 10:59:32 -0500

    Update 3.2_Trapezoid.py

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,9 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+
+base1 = int(input("What is the first base? : "))
+base2 = int(input("What is the second base? : "))
+height = int(input("What is the height? : "))
+area = ((base1 + base2) / 2) * height
+print(area)
